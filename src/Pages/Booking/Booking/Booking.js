07aef5f48d55d1620f45a91ff42471591bec0f45
zxashik <zxashik@gmail.com>
2021-10-20 14:56:46 +0600

    update

--- src/Pages/Booking/Booking/Booking.js
+++ src/Pages/Booking/Booking/Booking.js
@@ -1,30 +1,44 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import './Booking.css'
+import { NavLink } from 'react-router-dom';
 
-const Booking = () => {
+const Booking = (services) => {
 
     const { serviceId } = useParams();
 
-    const [users, setUsers] = useState([]);
-    const [singleUser, setSingleUser] = useState([]);
+    const [users, setUsers] = useState({});
+    // const [singleUser, setSingleUser] = useState([]);
 
     useEffect(() => {
         fetch('/services.json')
             .then(res => res.json())
-            .then(data => setUsers(data))
+            .then(data => {
+                const service = data.find(user => user?.id === parseInt(serviceId));
+                setUsers(service);
+            })
     }, [])
 
-    useEffect(() => {
-        const foundUser = users.find(user => user?.id === serviceId);
-        setSingleUser(foundUser);
-    }, [users]);
+    console.log({ users });
+    console.log({ serviceId });
+
+
 
     return (
         <div>
 
-            <h2>Service details {serviceId}</h2>
-            <h1>name {singleUser?.name}</h1>
+            <div className="container my-5">
+                {/* <h2>Service details {serviceId}</h2> */}
+                <h1>{users.name}</h1>
+                <img src={users.img} alt="" />
+                <p className="w-50">{users.description}</p>
+
+                <div className="my-5">
+                    <NavLink className="mx-2 home-btn" exact to="/" style={{ fontWeight: "bold", color: "black", textDecoration: "none" }} activeStyle={{ fontWeight: "bold", color: "orange", textDecoration: "none" }}>BACK TO HOMEPAGE</NavLink>
+
+                </div>
+            </div>
+
         </div >
     );
 };
